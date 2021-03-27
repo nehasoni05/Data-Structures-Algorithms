dd2b9957c02055c313b59db5487fad4d023a5de0
Neha Soni <67725607+nehasoni05@users.noreply.github.com>
2021-03-27 17:22:46 +0530

    Update rotateRight.java

--- Arrays/rotateRight.java
+++ Arrays/rotateRight.java
@@ -4,7 +4,7 @@ An array is said to be right rotated if all elements of the array are moved to i
 One approach is to loop through the array by shifting each element of the array to its next position.
 The last element of the array will become the first element of the rotated array.
 */
-class RotateRight 
+class rotateRight 
 {    
     public static void main(String[] args) 
     {    
